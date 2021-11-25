39f3c40c45656946cf830391af80a6535651f8e7
jaballogian <jaballogianrma77@gmail.com>
2021-11-25 21:54:28 +0700

    add fetch get covid country list api function in Data Grid page

--- src/pages/DataGrid/DataGrid.jsx
+++ src/pages/DataGrid/DataGrid.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+// SERVICES
+import { getCovidCountryListApi } from 'services/covid/getCovidApi'
 
 // STYLES
 import useStyles from './dataGridUseStyles'
 
 const DataGrid = () => {
   const classes = useStyles()
+
+  const fetchGetCovidCountryListApi = async () => {
+    const data = await getCovidCountryListApi()
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchGetCovidCountryListApi()
+  }, [])
 
   return (
     <div className={classes.dataGridroot}>
