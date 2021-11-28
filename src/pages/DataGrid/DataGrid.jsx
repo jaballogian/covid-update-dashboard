03f46f00499cbe7731ac8b91ea4edd5f779ba35a
jaballogian <jaballogianrma77@gmail.com>
2021-11-28 10:53:44 +0700

    add columns in Data Grid component

--- src/pages/DataGrid/DataGrid.jsx
+++ src/pages/DataGrid/DataGrid.jsx
@@ -9,6 +9,64 @@ import useStyles from './dataGridUseStyles'
 const DataGrid = () => {
   const classes = useStyles()
 
+  const columns = [
+    {
+      field: 'no',
+      headerName: '#',
+      flex: 0,
+      width: 100,
+    },
+    // FORMATTED COUNTRY HERE,
+    {
+      field: 'formatted_latest_confirmed',
+      headerName: 'Total Cases',
+      flex: 1,
+      minWidth: 200,
+    },
+    {
+      field: 'formatted_today_confirmed',
+      headerName: 'Today New Cases',
+      flex: 1,
+      minWidth: 200,
+    },
+    {
+      field: 'formatted_latest_deaths',
+      headerName: 'Total Deaths',
+      flex: 1,
+      minWidth: 200,
+    },
+    {
+      field: 'formatted_today_deaths',
+      headerName: 'Today New Deaths',
+      flex: 1,
+      minWidth: 200,
+    },
+    {
+      field: 'formatted_latest_recovered',
+      headerName: 'Total Recovered',
+      flex: 1,
+      minWidth: 200,
+    },
+    {
+      field: 'formatted_death_rate',
+      headerName: 'Death Rate',
+      flex: 1,
+      minWidth: 200,
+    },
+    {
+      field: 'formatted_recovery_rate',
+      headerName: 'Recovery Rate',
+      flex: 1,
+      minWidth: 200,
+    },
+    {
+      field: 'population',
+      headerName: 'Population',
+      flex: 1,
+      minWidth: 200,
+    },
+  ]
+
   const fetchFormatGetCovidCountryListApi = async () => {
     let data = await fotmatGetCovidCountryListApi()
 
