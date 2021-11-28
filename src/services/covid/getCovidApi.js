d197f4aae94cb1300923b8e63aa2a029fc3a41f5
jaballogian <jaballogianrma77@gmail.com>
2021-11-28 10:18:38 +0700

    add format get covid country list api function in get covid api service

--- src/services/covid/getCovidApi.js
+++ src/services/covid/getCovidApi.js
@@ -10,3 +10,19 @@ export const getCovidCountryListApi = async () => {
   console.log('responseJson', responseJson)
   return responseJson
 }
+
+export const fotmatGetCovidCountryListApi = async () => {
+  const rawData = await getCovidCountryListApi()
+  const output = rawData.data.map(item => {
+    return {
+      ...item,
+      today_deaths: item.today.deaths,
+      today_confirmed: item.today.confirmed,
+      latest_deaths: item.latest_data.deaths,
+      latest_confirmed: item.latest_data.confirmed,
+      latest_recovered: item.latest_data.recovered,
+      latest_critical: item.latest_data.critical,
+    }
+  })
+  return output
+}
