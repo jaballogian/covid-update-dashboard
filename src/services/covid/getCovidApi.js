6eb5a6d330126bbb4a71fb8519684dd0e5568b1a
jaballogian <jaballogianrma77@gmail.com>
2021-11-28 13:41:32 +0700

    change the output for format get covid country list api function in get covid api service

--- src/services/covid/getCovidApi.js
+++ src/services/covid/getCovidApi.js
@@ -17,18 +17,14 @@ export const fotmatGetCovidCountryListApi = async () => {
     return {
       ...item,
       id: item.code,
-      formatted_country: {
-        name: item.name,
-        code: item.code,
-      },
       formatted_today_deaths: item.today.deaths,
       formatted_today_confirmed: item.today.confirmed,
       formatted_latest_deaths: item.latest_data.deaths,
       formatted_latest_confirmed: item.latest_data.confirmed,
       formatted_latest_recovered: item.latest_data.recovered,
       formatted_latest_critical: item.latest_data.critical,
-      formatted_death_rate: item.latest_data.death_rate,
-      formatted_recovery_rate: item.latest_data.recovery_rate,
+      formatted_death_rate: item.latest_data.calculated.death_rate,
+      formatted_recovery_rate: item.latest_data.calculated.recovery_rate,
     }
   })
   return output
