39238468c8e9ed5e2ca21f6fcedee8999007da5e
jaballogian <jaballogianrma77@gmail.com>
2021-12-11 22:46:45 +0700

    wrap Main page with Page Main Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
+// CONTEXTS
+import { PageMainContextProvider } from 'contexts/PageMainContext'
+
 // PAGES
 import Main from 'pages/Main/Main'
 
 function App() {
   return (
     <div>
-      <Main/>
+      <PageMainContextProvider>
+        <Main/>
+      </PageMainContextProvider>
     </div>
   )
 }
