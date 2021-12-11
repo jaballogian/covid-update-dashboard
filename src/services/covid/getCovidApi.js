9a9943b54625f3337e8667fa17bb691dd319f9f1
jaballogian <jaballogianrma77@gmail.com>
2021-12-11 19:28:35 +0700

    change get covid country list api and format get covid country list api functions in get covid api service

--- src/services/covid/getCovidApi.js
+++ src/services/covid/getCovidApi.js
@@ -1,28 +1,20 @@
-export const getCovidCountryListApi = async () => {
+export const getYesterdayCovidCountryListApi = async () => {
   const baseUrl = process.env.REACT_APP_API_CORONA_BASE_URL
-  const completeUrl = `${baseUrl}/countries`
+  const completeUrl = `${baseUrl}countries?yesterday&sort`
   
   const response = await fetch(completeUrl, {
     method: 'GET',
   })
 
   const responseJson = await response.json()
-  console.log('responseJson', responseJson)
   return responseJson
 }
 
-export const fotmatGetCovidCountryListApi = (inputData) => {
+export const fotmatGetYesterdayCovidCountryListApi = (inputData) => {
   const output = inputData.map(item => {
     return {
       ...item,
-      id: item.code,
-      formatted_today_deaths: item.today.deaths,
-      formatted_today_confirmed: item.today.confirmed,
-      formatted_latest_deaths: item.latest_data.deaths,
-      formatted_latest_confirmed: item.latest_data.confirmed,
-      formatted_latest_recovered: item.latest_data.recovered,
-      formatted_death_rate: item.latest_data.calculated.death_rate,
-      formatted_recovery_rate: item.latest_data.calculated.recovery_rate,
+      id: item.country,
     }
   })
   return output
