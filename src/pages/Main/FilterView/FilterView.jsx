113589d7ec9a0ab6551b12bd189efde6478d1967
jaballogian <jaballogianrma77@gmail.com>
2021-12-11 22:54:44 +0700

    replace dummy country list with covid country list data

--- src/pages/Main/FilterView/FilterView.jsx
+++ src/pages/Main/FilterView/FilterView.jsx
@@ -1,43 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+
+// CONTEXTS
+import { PageMainContext } from 'contexts/PageMainContext'
 
 // MUI
 import Autocomplete from '@mui/material/Autocomplete'
 import TextField from '@mui/material/TextField'
 
 const FilterView = () => {
-  const [ search, setSearch ] = useState(null)
+  const { covidCountryListData } = useContext(PageMainContext)
 
-  const dummyCountryList = [
-    {
-      country: 'Afghanistan',
-      countryInfo: {
-        '_id': 4,
-        'iso2': 'AF',
-        'iso3': 'AFG',
-        'lat': 33,
-        'long': 65,
-        'flag': 'https://disease.sh/assets/img/flags/af.png'
-      },
-    },
-    {
-      country: 'Albania',
-      countryInfo: {
-        '_id': 8,
-        'iso2': 'AL',
-        'iso3': 'ALB',
-        'lat': 41,
-        'long': 20,
-        'flag': 'https://disease.sh/assets/img/flags/al.png'
-      },
-    },
-  ]
+  const [ search, setSearch ] = useState(null)
 
   return (
     <>
       <Autocomplete
         value={search}
         onChange={(event, newValue) => setSearch(newValue)}
-        options={dummyCountryList}
+        options={covidCountryListData}
         getOptionLabel={(option) => option.country}
         renderOption={(props, option) => (
           <div {...props}>
