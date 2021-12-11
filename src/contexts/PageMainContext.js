ed7228cf8d7921f5471da8db27c012bb00a29493
jaballogian <jaballogianrma77@gmail.com>
2021-12-11 22:52:44 +0700

    add covid country list data state variable and its function

--- src/contexts/PageMainContext.js
+++ src/contexts/PageMainContext.js
@@ -1,11 +1,19 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 const PageMainContext = createContext()
 
 const PageMainContextProvider = (props) => {
+  // ARRAY
+  const [ covidCountryListData, setCovidCountryListData ] = useState([])
+
+  const changeCovidCountryListData = (inputListData) => {
+    setCovidCountryListData(inputListData)
+  }
+
   return (
     <PageMainContext.Provider
       value={{
+        covidCountryListData, changeCovidCountryListData,
       }}
     >
       {props['children']}
