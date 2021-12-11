e5d869361c7353a2529fc5fa85f45f9dcf71d6f7
jaballogian <jaballogianrma77@gmail.com>
2021-12-11 22:22:47 +0700

    add dummy county list

--- src/pages/Main/FilterView/FilterView.jsx
+++ src/pages/Main/FilterView/FilterView.jsx
@@ -1,6 +1,31 @@
 import React from 'react'
 
 const FilterView = () => {
+  const dummyCountryList = [
+    {
+      country: 'Afghanistan',
+      countryInfo: {
+        '_id': 4,
+        'iso2': 'AF',
+        'iso3': 'AFG',
+        'lat': 33,
+        'long': 65,
+        'flag': 'https://disease.sh/assets/img/flags/af.png'
+      },
+    },
+    {
+      country: 'Albania',
+      countryInfo: {
+        '_id': 8,
+        'iso2': 'AL',
+        'iso3': 'ALB',
+        'lat': 41,
+        'long': 20,
+        'flag': 'https://disease.sh/assets/img/flags/al.png'
+      },
+    },
+  ]
+
   return (
     <div>
       Filter View
