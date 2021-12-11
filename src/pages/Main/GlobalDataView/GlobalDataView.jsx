bf7a265ba4c40bcd5ca7278b3ecd2abb059594de
jaballogian <jaballogianrma77@gmail.com>
2021-12-11 21:47:10 +0700

    add fetch get yesterday covid global data function in Global Data View component

--- src/pages/Main/GlobalDataView/GlobalDataView.jsx
+++ src/pages/Main/GlobalDataView/GlobalDataView.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+// SERVICES
+import { getYesterdayCovidGlobalData } from 'services/covid/getCovidApi'
 
 const GlobalDataView = () => {
+  const fetchGetYesterdayCovidGlobalData = async () => {
+    const data = await getYesterdayCovidGlobalData()
+  }
+
+  useEffect(() => {
+    fetchGetYesterdayCovidGlobalData()
+  }, [])
+
   return (
     <div>
       Global Data View
