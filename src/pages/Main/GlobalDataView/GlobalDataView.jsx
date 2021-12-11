2db0e3fb75972cd3aabf00ef9244e24ad7be2d96
jaballogian <jaballogianrma77@gmail.com>
2021-12-11 22:05:52 +0700

    map card list value into view

--- src/pages/Main/GlobalDataView/GlobalDataView.jsx
+++ src/pages/Main/GlobalDataView/GlobalDataView.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
+// MUI
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+
 // SERVICES
 import { getYesterdayCovidGlobalData } from 'services/covid/getCovidApi'
 
@@ -32,9 +37,30 @@ const GlobalDataView = () => {
   }, [])
 
   return (
-    <div>
-      Global Data View
-    </div>
+    <>
+      {cardList &&
+      <Grid container>
+        {cardList.map((item, index) => (
+          <Grid 
+            key={index}
+            xs={12}
+            md={4}
+          >
+            <Card>
+              {/* TITLE */}
+              <Typography>
+                {item.title}
+              </Typography>
+
+              {/* VAlUE */}
+              <Typography>
+                {item.value}
+              </Typography>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>}
+    </>
   )
 }
 
