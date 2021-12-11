2b89872405ffa12d442e765945804b0a5f400e25
jaballogian <jaballogianrma77@gmail.com>
2021-12-11 21:48:49 +0700

    add global data state variable and its function in Global Data View component

--- src/pages/Main/GlobalDataView/GlobalDataView.jsx
+++ src/pages/Main/GlobalDataView/GlobalDataView.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 // SERVICES
 import { getYesterdayCovidGlobalData } from 'services/covid/getCovidApi'
 
 const GlobalDataView = () => {
+  const [ globalData, setGlobalData ] = useState(null)
+
   const fetchGetYesterdayCovidGlobalData = async () => {
     const data = await getYesterdayCovidGlobalData()
+    setGlobalData(data)
   }
 
   useEffect(() => {
