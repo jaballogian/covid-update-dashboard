9046068819052feae79c17e823b0fdd7fdb63426
jaballogian <jaballogianrma77@gmail.com>
2021-12-11 21:31:44 +0700

    add Main page and its styles

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// USE STYLES
+import useStyles from './mainUseStyles'
+
+const Main = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Main
