0a800609db0e678943b60f0bdc9826dd2be9da5d
jaballogian <jaballogianrma77@gmail.com>
2021-12-11 21:37:48 +0700

    add Table And List View component in Main page

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
+// COMPONENTS
+import TableAndListView from './TableAndListView/TableAndListView'
+
 // USE STYLES
 import useStyles from './mainUseStyles'
 
 const Main = () => {
   return (
     <div>
-      
+      <TableAndListView/>
     </div>
   )
 }
