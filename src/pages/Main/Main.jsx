a573ce30a6419e6968546daad0b539a137fad96d
jaballogian <jaballogianrma77@gmail.com>
2021-12-11 22:12:59 +0700

    add Filter View component

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 // COMPONENTS
 import GlobalDataView from './GlobalDataView/GlobalDataView'
+import FilterView from './FilterView/FilterView'
 import TableView from './TableView/TableView'
 
 const Main = () => {
   return (
     <div>
       <GlobalDataView/>
+      <FilterView/>
       <TableView/>
     </div>
   )
