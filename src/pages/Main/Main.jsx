538289ba752a7294ba6a4ae6848354f7cd7ae912
jaballogian <jaballogianrma77@gmail.com>
2021-12-11 21:44:04 +0700

    add Global Data View component

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 // COMPONENTS
+import GlobalDataView from './GlobalDataView/GlobalDataView'
 import TableView from './TableView/TableView'
 
 const Main = () => {
   return (
     <div>
+      <GlobalDataView/>
       <TableView/>
     </div>
   )
