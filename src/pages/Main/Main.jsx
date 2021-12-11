f312d612e02c43e645f5ff5d18c2f6ecb41d517c
jaballogian <jaballogianrma77@gmail.com>
2021-12-11 21:42:23 +0700

    remove the style file for Main page

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,9 +3,6 @@ import React from 'react'
 // COMPONENTS
 import TableView from './TableView/TableView'
 
-// USE STYLES
-import useStyles from './mainUseStyles'
-
 const Main = () => {
   return (
     <div>
--- src/pages/Main/mainUseStyles.js
+++
@@ -1,7 +0,0 @@
-// MATERIAL UI STYLES
-import { makeStyles } from '@mui/styles'
-
-const useStyles = makeStyles((theme) => ({
-}))
-
-export default useStyles
