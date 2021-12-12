5b6b2d66315acb1eeb99ce6e9170008a629c8745
jaballogian <jaballogianrma77@gmail.com>
2021-12-12 15:57:39 +0700

    wrap App component with Theme Provider, Styled Engine Provider, and All Pages Context Provider components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+// COMPONENTS
 import App from './App'
+
+// CONSTANTS
+import customTheme from 'constants/customTheme'
+
+// CONTEXTS
+import { AllPagesContextProvider } from 'contexts/AllPagesContext'
+
+// MATERIAL UI
+import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles'
 
 // STYLES
 import './index.css'
 
 ReactDOM.render(
-  <App />,
+  <ThemeProvider theme={customTheme}>
+    <StyledEngineProvider injectFirst>
+      <AllPagesContextProvider>
+        <App/>
+      </AllPagesContextProvider>
+    </StyledEngineProvider>
+  </ThemeProvider>,
   document.getElementById('root')
 )
