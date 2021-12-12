7bd9651fe81c8472818c00651cf10b09e176c13b
jaballogian <jaballogianrma77@gmail.com>
2021-12-12 15:55:48 +0700

    create custom theme constant

---
+++ src/constants/customTheme.js
@@ -0,0 +1,8 @@
+// MATERIAL UI CORES
+import { createTheme, responsiveFontSizes } from '@mui/material/styles'
+
+let customTheme = createTheme({})
+
+customTheme = responsiveFontSizes(customTheme) 
+
+export default customTheme
