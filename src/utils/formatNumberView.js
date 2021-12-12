47238510213b205d524631b6bd2478f3f01e2b04
jaballogian <jaballogianrma77@gmail.com>
2021-12-12 15:18:09 +0700

    change abbreviate number function

--- src/utils/formatNumberView.js
+++ src/utils/formatNumberView.js
@@ -1,7 +1,7 @@
 export const abbreviateNumber = (inputNumber) => {
   if (inputNumber < 1e3) return inputNumber
-  if (inputNumber >= 1e3 && inputNumber < 1e6) return +(inputNumber / 1e3).toFixed(1) + ' K'
-  if (inputNumber >= 1e6 && inputNumber < 1e9) return +(inputNumber / 1e6).toFixed(1) + ' M'
-  if (inputNumber >= 1e9 && inputNumber < 1e12) return +(inputNumber / 1e9).toFixed(1) + ' B'
-  if (inputNumber >= 1e12) return +(inputNumber / 1e12).toFixed(1) + ' T'
+  if (inputNumber >= 1e3 && inputNumber < 1e6) return (Math.floor(inputNumber / 100)) / 10 + ' K'
+  if (inputNumber >= 1e6 && inputNumber < 1e9) return (Math.floor(inputNumber / 1e5)) / 10 + ' M'
+  if (inputNumber >= 1e9 && inputNumber < 1e12) return (Math.floor(inputNumber / 1e8)) / 10 + ' B'
+  if (inputNumber >= 1e12) return (Math.floor(inputNumber / 1e11)) / 10 + ' T'
 }
