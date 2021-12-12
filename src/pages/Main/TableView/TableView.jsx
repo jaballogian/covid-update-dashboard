bd4997331715ec61a1f1ca1a9dcbc0c0522a63a0
jaballogian <jaballogianrma77@gmail.com>
2021-12-12 09:45:20 +0700

    add render cell prop for today recovered column

--- src/pages/Main/TableView/TableView.jsx
+++ src/pages/Main/TableView/TableView.jsx
@@ -125,6 +125,13 @@ const TableView = () => {
       headerName: 'Today Recovered',
       flex: 1,
       minWidth: 150,
+      renderCell: (params) => (
+        <Typography variant='subtitle2'>
+          {abbreviatedOrDetail === 'abbreviated' ?
+          abbreviateNumber(params.value) :
+          params.value}
+        </Typography>
+      ),
     },
     {
       field: 'tests',
