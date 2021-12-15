44894e7d351e060c5099785b833dcd76b7e639bb
jaballogian <jaballogianrma77@gmail.com>
2021-12-15 07:55:42 +0700

    map covid country list data into a view

--- src/pages/Main/ListView/ListView.jsx
+++ src/pages/Main/ListView/ListView.jsx
@@ -3,13 +3,41 @@ import React, { useContext } from 'react'
 // CONTEXTS
 import { PageMainContext } from 'contexts/PageMainContext'
 
+// MUI
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+
+// STYLES
+import useStyles from './listViewUseStyles'
+
 const ListView = () => {
   const { covidCountryListData } = useContext(PageMainContext)
 
+  const classes = useStyles()
+
   return (
-    <div>
-      List View
-    </div>
+    <Grid container>
+      {covidCountryListData.map((item, index) => (
+        <Grid
+          key={index}
+          item
+          xs={12} sm={6}
+        >
+          {/* FLAG AND COUNTRY */}
+          <div className={classes.flexContainer}>
+            {item.countryInfo._id &&
+            <img
+              src={item.countryInfo.flag}
+              alt=''
+              className={classes.dataGridCountryFlag}
+            />}
+            <Typography>
+              {item.country}
+            </Typography>
+          </div>
+        </Grid>
+      ))}
+    </Grid>
   )
 }
 
---
+++ src/pages/Main/ListView/listViewUseStyles.js
@@ -0,0 +1,14 @@
+// MATERIAL UI STYLES
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles((theme) => ({
+  flexContainer: {
+    display: 'flex',
+  },
+  dataGridCountryFlag: {
+    width: 30,
+    marginRight: 20,
+  },
+}))
+
+export default useStyles
