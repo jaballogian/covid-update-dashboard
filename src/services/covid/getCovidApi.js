cf679ef49c35ba7835abb9209e8ad0ac56e2ce39
jaballogian <jaballogianrma77@gmail.com>
2021-12-18 20:57:53 +0700

    add get historical covid global data function

--- src/services/covid/getCovidApi.js
+++ src/services/covid/getCovidApi.js
@@ -21,3 +21,19 @@ export const getYesterdayCovidGlobalData = async () => {
   const responseJson = await response.json()
   return responseJson
 }
+
+export const getHistoricalCovidGlobalData = async () => {
+  const firstRecordDate = '01/01/2020'
+  const timeDifference = new Date().getTime() - new Date(firstRecordDate).getTime()
+  const totalDaysCount = Math.ceil(timeDifference / (1000 * 3600 * 24))
+
+  const baseUrl = process.env.REACT_APP_API_CORONA_BASE_URL
+  const completeUrl = `${baseUrl}historical/all?lastdays=${totalDaysCount}`
+  
+  const response = await fetch(completeUrl, {
+    method: 'GET',
+  })
+
+  const responseJson = await response.json()
+  return responseJson
+}
