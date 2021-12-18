d2dd3aa6b9b6604f4ae9a7075431f801cae0bd96
jaballogian <jaballogianrma77@gmail.com>
2021-12-18 20:58:13 +0700

    add fetch get historical covid global data function

--- src/pages/Main/GlobalGraphView/GlobalGraphView.jsx
+++ src/pages/Main/GlobalGraphView/GlobalGraphView.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+// SERVICES
+import { getHistoricalCovidGlobalData } from 'services/covid/getCovidApi'
 
 const GlobalGraphView = () => {
+  const fetchGetHistoricalCovidGlobalData = async () => {
+    const data = await getHistoricalCovidGlobalData()
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchGetHistoricalCovidGlobalData()
+
+    return () => fetchGetHistoricalCovidGlobalData()
+  }, [])
+  
   return (
     <div>
       Global Graph View
