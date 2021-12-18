cf1b69cf54c5e03c3d7af71ef232e060c5320172
jaballogian <jaballogianrma77@gmail.com>
2021-12-18 22:14:35 +0700

    add difference consecutive numbers in array function

--- src/utils/formatNumberView.js
+++ src/utils/formatNumberView.js
@@ -5,3 +5,8 @@ export const abbreviateNumber = (inputNumber) => {
   if (inputNumber >= 1e9 && inputNumber < 1e12) return (Math.floor(inputNumber / 1e8)) / 10 + ' B'
   if (inputNumber >= 1e12) return (Math.floor(inputNumber / 1e11)) / 10 + ' T'
 }
+
+export const differenceConsecutiveNumbersInArray = (inputArray) => {
+  let output = inputArray.slice(1).map((item, index) => item - inputArray[index])
+  return [inputArray[0], ...output]
+}
